a984372e2f652df7cc81e7fe283b0a56fb5fbabe
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-04-05 19:39:13 -0400

    create and init event-bus service

---
+++ event-bus/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const PORT = process.env.PORT || 8085;
+
+// init
+const app = express();
+
+// add middlewares
+app.use(bodyParser.json());
+
+// base route
+app.get("/", (_, res) => {
+  res.send("Hello from event-bus service!");
+});
+
+// additional routes
+app.post("/events", (req, res, _next) => {
+  const event = req.body;
+
+  // send requests
+  axios.post("http://localhost:8080", event);
+  axios.post("http://localhost:8081", event);
+  axios.post("http://localhost:8082", event);
+
+  res.send({ status: "OK" });
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
