25f68bdbf95c9551d9667caa098c951a12dece9b
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-04-18 10:11:49 -0400

    add query interfaces

---
+++ query/src/interfaces.ts
@@ -0,0 +1,36 @@
+export class EventType<D extends object> {
+  type: string;
+  data: D;
+
+  constructor(type: string, data: D) {
+    this.type = type;
+    this.data = data;
+  }
+}
+export type PostIdKey = string;
+
+export class Comment {
+  id: string;
+  text: string;
+  postId: string;
+
+  constructor(id: string, text: string, postId: PostIdKey) {
+    this.id = id;
+    this.text = text;
+    this.postId = postId;
+  }
+}
+export class Post {
+  id: string;
+  title: string;
+  comments: Comment[] = [];
+
+  constructor(id: string, title: string, comments?: Comment[]) {
+    this.id = id;
+    this.title = title;
+    this.comments = comments || [];
+  }
+}
+
+export class EventCommentCreated extends EventType<Comment> {}
+export class EventPostCreated extends EventType<Post> {}
