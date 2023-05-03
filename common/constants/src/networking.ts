f02627048eed2d6fffdc8a18b7bb74f81598dc4e
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-05-03 18:25:10 -0400

    infra- fix endpoints

--- common/constants/src/networking.ts
+++ common/constants/src/networking.ts
@@ -18,16 +18,16 @@ export const HOSTNAMES = {
   eventBus: { internal: "http://event-bus-srv" },
   moderation: { internal: "http://moderation-srv" },
   query: {
-    external: "http://query-clusterip-srv",
+    external: "http://posts.docker",
     internal: "http://query-srv",
   },
 };
 
-export const HOST_ADDR_MODERATION = `${HOSTNAMES.moderation.internal}:${PORTS.moderation}`;
-export const HOST_ADDR_EVENT_BUS = `${HOSTNAMES.eventBus.internal}:${PORTS.eventBus}`;
-export const HOST_ADDR_POSTS_EXTERNAL = `${HOSTNAMES.posts.external}:${PORTS.posts}`;
-export const HOST_ADDR_POSTS_INTERNAL = `${HOSTNAMES.posts.internal}:${PORTS.posts}`;
-export const HOST_ADDR_COMMENTS_EXTERNAL = `${HOSTNAMES.comments.external}:${PORTS.comments}`;
-export const HOST_ADDR_COMMENTS_INTERNAL = `${HOSTNAMES.comments.internal}:${PORTS.comments}`;
-export const HOST_ADDR_QUERY_EXTERNAL = `${HOSTNAMES.query.external}:${PORTS.query}`;
-export const HOST_ADDR_QUERY_INTERNAL = `${HOSTNAMES.query.internal}:${PORTS.query}`;
+export const HOST_ADDR_MODERATION = `${HOSTNAMES.moderation.internal}`;
+export const HOST_ADDR_EVENT_BUS = `${HOSTNAMES.eventBus.internal}`;
+export const HOST_ADDR_POSTS_EXTERNAL = `${HOSTNAMES.posts.external}`;
+export const HOST_ADDR_POSTS_INTERNAL = `${HOSTNAMES.posts.internal}`;
+export const HOST_ADDR_COMMENTS_EXTERNAL = `${HOSTNAMES.comments.external}`;
+export const HOST_ADDR_COMMENTS_INTERNAL = `${HOSTNAMES.comments.internal}`;
+export const HOST_ADDR_QUERY_EXTERNAL = `${HOSTNAMES.query.external}`;
+export const HOST_ADDR_QUERY_INTERNAL = `${HOSTNAMES.query.internal}`;
